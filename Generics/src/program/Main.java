6f2b24538961f31b2629ca2f52bb6f075783cad3
caioglauber <caioglauber@gmail.com>
2026-01-28 16:40:02 -0300

    Generics Classes

---
+++ Generics/src/program/Main.java
@@ -0,0 +1,27 @@
+
+package program;
+
+import entities.PrintService;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        PrintService<Integer> ps = new PrintService<>();
+        
+        System.out.println("How many values: ");
+        int values = sc.nextInt();
+        
+        for(int i = 0;i< values;i++){
+            Integer n = sc.nextInt();
+            
+            ps.addValeu(n);
+            
+        }
+        ps.print();
+        System.out.println("First: " + ps.first());
+        
+        
+    }
+}
